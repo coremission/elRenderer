1968088c3638aed4082c6253125bc6830d5da905
unknown <skormishin@kormishin.eamobile.ad.ea.com>
2015-09-08 19:46:35 +0300

    first initial commit

---
+++ ElRenderer/BitmapExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace ElRenderer
+{
+    public static class BitmapExtensions
+    {
+        public static void elDrawLine(this Bitmap bitmap, int xStart, int yStart, int xEnd, int yEnd) {
+            bitmap.SetPixel(xStart, yStart, Color.White);
+            bitmap.SetPixel(xEnd, yEnd, Color.White);
+
+            for (int i = 300; i < 500; i++)
+            {
+                bitmap.SetPixel(100, i, Color.White);
+            }
+        }
+
+    }
+}
---
+++ ElRenderer/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElRenderer
+{
+    public class MainForm : Form
+    {
+        private const int WIDTH = 1024;
+        private const int HEIGHT = 768;
+
+        private void InitForm()
+        {
+            this.ClientSize = new System.Drawing.Size(WIDTH, HEIGHT);
+            this.Name = "MainForm";
+            this.Text = "el Renderer";
+            this.BackColor = Color.Black;
+        }
+
+        Bitmap screen;
+
+        public MainForm()
+        {
+            InitForm();
+            screen = new Bitmap(WIDTH, HEIGHT);
+            
+            screen.elDrawLine(50, 50, 200, 200);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            e.Graphics.DrawImage(screen, 0, 0);
+        }
+    }
+}
