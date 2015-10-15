a9cfc87c2fe27841b5864c1082b37ebeead2df6b
Sergey <s.coremission@gmail.com>
2015-10-15 17:22:39 +0300

    deleted cached files, updated .gitignore, added math tests, quaternion class

---
+++ ElRenderer/Algebraic/Quaternion.cs
@@ -0,0 +1,39 @@
+﻿
+namespace ElRenderer.Algebraic
+{
+    public class Quaternion
+    {
+        public float w;
+        public float x;
+        public float y;
+        public float z;
+
+        public Quaternion(float w, float x, float y, float z)
+        {
+            this.w = w;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static Quaternion operator +(Quaternion a, Quaternion b)
+        {
+            return new Quaternion(a.w + b.w, a.x + b.x, a.y + b.y, a.z + b.z);
+        }
+
+        public static bool operator !=(Quaternion a, Quaternion b)
+        {
+            return !(a == b);
+        }
+
+        public static bool operator ==(Quaternion a, Quaternion b)
+        {
+            return (a.w == b.w && a.x == b.x && a.y == b.y && a.z == b.z);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+    }
+}
---
+++ MathTests/QuaternionsTestFixture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ElRenderer.Algebraic;
+
+namespace MathTests
+{
+    [TestClass]
+    public class QuaternionsTestFixture
+    {
+        [TestMethod]
+        public void AdditionTest()
+        {
+            Quaternion q = new Quaternion(0, 0, 0, 0);
+            Assert.IsTrue(q + q == new Quaternion(0, 0, 0, 0), "Zero quaternion is identity");
+        }
+    }
+}
